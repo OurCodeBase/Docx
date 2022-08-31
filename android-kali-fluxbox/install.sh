09f82247aab992eb4692b8f1ac29a1b3c3b840d0
Harsh B <100824266+harsh7i@users.noreply.github.com>
2022-08-31 21:20:19 +0530

    update install.sh

--- android-kali-fluxbox/install.sh
+++ android-kali-fluxbox/install.sh
@@ -12,4 +12,33 @@ Success="\e[0;32m[+]"
 dim="\e[0;2m"
 enc="\e[0;m"
 
-echo -e "$dim" && echo -e "$Success Installing Requirements $dim" && echo -e "$dim" && sudo apt update && echo -e "$dim" && echo -e "$Red   apt is running...$dim" && echo -e "$dim" && sudo apt install tigervnc-standalone-server -y && echo -e "$dim" && echo -e "$Red  installing vncserver...$dim" && echo -e "$dim" && sudo apt install tigervnc-standalone-server -y && echo -e "$dim" && echo -e "$Red  installing fluxbox...$dim" && echo -e "$dim" && sudo apt install fluxbox -y && sudo apt remove twm tightvncserver -y && sudo apt autoremove -y && echo -e "$enc $dim" && curl -OL https://raw.githubusercontent.com/harsh7i/Docx/main/android-kali-fluxbox/vncstart && curl -OL https://raw.githubusercontent.com/harsh7i/Docx/main/android-kali-fluxbox/vncstop && mv vncst* /usr/bin && chmod +x /usr/bin/vncstart && chmod +x /usr/bin/vncstop &&  echo "" && echo -e "$Success Successfully setup completed $enc" && echo "" && vncserver -geometry 1280x720 -xstartup /usr/bin/startfluxbox && echo "" && echo -e "\e[0;32m[+] Server successfully running at localhost:1  \e[0;m" && echo ""
+echo ""
+echo -e "$Success Installing Requirements...$enc"
+echo ""
+echo -e "$Red   apt is running...$enc"
+sudo apt update -y >> /dev/null
+echo ""
+echo -e "$Red  installing vncserver...$enc"
+echo ""
+sudo apt install tigervnc-standalone-server -y >> /dev/null
+echo ""
+echo -e "$Red  installing fluxbox...$enc"
+sudo apt install fluxbox -y >> /dev/null
+echo ""
+echo -e "$Red   removing unwanted files...$enc"
+sudo apt remove twm tightvncserver -y
+sudo apt autoremove -y
+echo ""
+echo -e "$Red   finishing files...$enc"
+curl -OL https://raw.githubusercontent.com/harsh7i/Docx/main/android-kali-fluxbox/vncstart >> /dev/null
+curl -OL https://raw.githubusercontent.com/harsh7i/Docx/main/android-kali-fluxbox/vncstop >> /dev/null
+mv vncst* /usr/bin
+chmod +x /usr/bin/vncstart
+chmod +x /usr/bin/vncstop
+echo ""
+echo -e "$Success Successfully setup completed... $enc"
+echo ""
+vncserver -geometry 1280x720 -xstartup /usr/bin/startfluxbox
+echo ""
+echo -e "$Success Server Successfully Running at localhost:1 $enc"
+echo ""
