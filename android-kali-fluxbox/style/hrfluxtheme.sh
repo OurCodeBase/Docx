05e91d038e613c3d5af5adf18993278c414abf5b
Harsh B <100824266+harsh7i@users.noreply.github.com>
2022-09-02 18:43:34 +0530

    update hrfluxtheme.sh

--- android-kali-fluxbox/style/hrfluxtheme.sh
+++ android-kali-fluxbox/style/hrfluxtheme.sh
@@ -37,7 +37,7 @@ if [[ "$proc" -lt "${#array[@]}" ]]; then
   echo -e "$enc"
   echo -e "$Success Extracting Files...$enc"
   echo ""
-  tar -xfv ${array[$proc]}.tar
+#  tar -xfv ${array[$proc]}.tar
   echo ""
   echo -e "$Success Removing Unwanted Files...$enc"
 #  rm ${array[$proc]}.tar
