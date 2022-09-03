70915350fc67e8bc365d068805765386277a4226
Harsh B <100824266+harsh7i@users.noreply.github.com>
2022-09-04 00:44:12 +0530

    update install-x.sh

--- android-kali-gui/install-x.sh
+++ android-kali-gui/install-x.sh
@@ -36,7 +36,7 @@ sudo clear
   mv vncst* /usr/bin
   chmod +x /usr/bin/vncst*
 } && {
-  vncserver -geometry 1280x720 -xstartup /usr/bin/xfce4-session
+  vncserver -geometry 1920x1080 -xstartup /usr/bin/xfce4-session
   echo ""
   echo -e "$Success Server successfully running on localhost:1 $enc"
   echo ""
