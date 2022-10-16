3c6f14595b4ce942269e2ab8fca5165122ee71d3
Harsh B <100824266+harsh7i@users.noreply.github.com>
2022-10-16 11:52:31 +0530

    Add files via upload

---
+++ android-kali-awesome/ui/menubar_prev.lua
@@ -0,0 +1,12 @@
+-- {{{ Menu
+-- Create a launcher widget and a main menu
+myawesomemenu = {
+   { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
+   { "manual", terminal .. " -e man awesome" },
+   { "edit config", editor_cmd .. " " .. awesome.conffile },
+   { "restart", awesome.restart },
+   { "quit", function() awesome.quit() end },
+}
+
+local menu_awesome = { "awesome", myawesomemenu, beautiful.awesome_icon }
+local menu_terminal = { "open terminal", terminal }
---
+++ android-kali-awesome/ui/script.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Variables
+Red="\e[0;31m"
+Green="\e[0;32m"
+Blue="\e[0;34m"
+Alert="\e[0;31m[×]"
+Success="\e[0;32m[+]"
+dim="\e[0;2m"
+enc="\e[0;m"
+
+clear
+echo ""
+echo -e "${Success} Installing Dependencies...${enc}"
+echo ""
+sudo apt install compton nitrogen -y
+echo ""
+{
+  if [[ -f "~/.config/awesome/rc.lua" ]]; then
+    #statements
+    echo -e "${Success} File already exists.${enc}"
+    echo ""
+  else
+    #statements
+    mkdir ~/.config &> /dev/null && mkdir ~/.config/awesome &> /dev/null
+    cp /etc/xdg/awesome/rc.lua ~/.config/awesome/
+    echo -e "${Success} File created successfully.${enc}"
+  fi
+} && {
+  echo ""
+} && {
+  if [[ -d "~/theme" ]]; then
+    #statements
+    echo -e "${Success} Theme dirs already exists.${enc}"
+    echo ""
+  else
+    mkdir ~/theme && ln -s /usr/share/awesome/themes/default/* ~/theme/
+    echo -e "${Success} Theme dirs successfully created.${enc}"
+    echo ""
+  fi
+} && {
+  echo -e "${Success} Configuration Started...${enc}"
+  echo ""
+  menu_adv=`cat menubar.lua`
+  menu_prev=`cat menubar_prev.lua`
+  sed -i 's/${menu_prev}/${menu_adv}/g' ~/.config/awesome/rc.lua
+  echo -e "\nawful.spawn.with_shell(compton)" >> ~/.config/awesome/rc.lua
+  echo -e "\nawful.spawn.with_shell(nitrogen --restore)" >> ~/.config/awesome/rc.lua
+}
