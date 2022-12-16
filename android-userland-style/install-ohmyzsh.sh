c05864fbfa22052114635d296b073f55fb1a3843
Harsh B <100824266+harsh7i@users.noreply.github.com>
2022-12-16 22:24:05 +0530

    Add files via upload

---
+++ android-userland-style/install-ohmyzsh.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Variables
+Alert="\e[0;31m[×]"
+Success="\e[0;32m[+]"
+enc="\e[0;m"
+
+zsh_syntax_on(){
+  echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
+}
+
+echo
+echo -e "${Success} Installing Oh-My-Zsh${enc}"
+echo
+{
+  apt install git -y
+} && {
+  git clone https://github.com/ohmyzsh/ohmyzsh.git "$HOME/.oh-my-zsh" --depth 1
+} && {
+  if [[ -f "$HOME/.zshrc" ]]; then
+    #statements
+    mv "$HOME/.zshrc" "$HOME/.zshrc.bak.$(date +%Y.%m.%d-%H:%M:%S)"
+  fi
+  cp "$HOME/.oh-my-zsh/templates/zshrc.zsh-template" "$HOME/.zshrc"
+} && {
+  echo
+  while true; do
+    read -p "Are you currently using zsh-syntax-highlighting ?" yn
+    case $yn in
+        [Yy]* ) zsh_syntax_on; break;;
+        [Nn]* ) echo -e "${Success} No problem${enc}";;
+        * ) echo "Please answer yes or no.";;
+    esac
+  done
+}
