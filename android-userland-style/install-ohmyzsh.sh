58c2ee2831af97788ac72cc9f9b6f7cf16e2231f
Harsh B <100824266+harsh7i@users.noreply.github.com>
2022-12-27 20:12:24 +0530

    update install-ohmyzsh.sh

--- android-userland-style/install-ohmyzsh.sh
+++ android-userland-style/install-ohmyzsh.sh
@@ -9,6 +9,12 @@ zsh_syntax_on(){
   echo "source $HOME/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "$HOME/.zshrc"
 }
 
+zsh_success(){
+  echo
+  echo -e "${Success} Please restart your userland session.${enc}"
+  echo
+}
+
 echo
 echo -e "${Success} Installing Oh-My-Zsh${enc}"
 echo
@@ -28,12 +34,8 @@ echo
     read -p "Are you currently using zsh-syntax-highlighting ?" yn
     case $yn in
         [Yy]* ) zsh_syntax_on; break;;
-        [Nn]* ) echo -e "${Success} No problem${enc}";;
+        [Nn]* ) zsh_success; ;;
         * ) echo "Please answer yes or no.";;
     esac
   done
-} && {
-  echo
-  echo -e "${Success} Please restart your userland session.${enc}"
-  echo
 }
